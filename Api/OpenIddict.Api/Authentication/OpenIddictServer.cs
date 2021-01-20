08aa637522b50893ab50839d3cd9f8eabf5a5a27
Adriel Santos <asantos@dataweb.com.br>
2021-01-20 17:07:10 -0300

    first commit

---
+++ Api/OpenIddict.Api/Authentication/OpenIddictServer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenIddcit.Api.Authentication
+{
+    public static class OpenIddictServer
+    {
+        public static void addOpenIddictServer(this IServiceCollection services, bool requireHttps)
+        {
+            services.AddOpenIddict()
+            .AddServer(options =>
+            {
+                // criando o endpoint para requisitar o token do OpenIddict
+                options.SetTokenEndpointUris("/api/connect/token");
+
+                
+                options.AllowPasswordFlow()
+                    .AllowRefreshTokenFlow();
+
+                //aceitando clientes anonimos (clientes que não enviam client_id)
+                options.AcceptAnonymousClients();
+
+
+
+            });    
+        }
+    }
+}
---
+++ Api/OpenIddict.Api/Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenIddcit.Api.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenIddcit.Api
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            _environment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+            services.addOpenIddictServer(!_environment.IsDevelopment());
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins("http://localhost:4200");
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+            });
+
+            //app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
